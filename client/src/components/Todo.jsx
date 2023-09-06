7a98730ee88a776284cde486f41ed028c6b8188a
paras-kumar-1086 <140137639+paras-kumar-1086@users.noreply.github.com>
2023-09-06 16:46:09 +0530

    Add files via upload

---
+++ client/src/components/Todo.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react"; 
+
+import { toggleTodo, updateTodo } from "../redux/actions";
+import { deleteTodo } from "../redux/actions";
+ 
+import { useDispatch } from "react-redux";
+
+const Todo = ({ todo }) => {
+
+    const [editing, setEditing] = useState(false);
+    const [text, setText] = useState(todo?.data);
+
+    const dispatch = useDispatch();
+    // eslint-disable-next-line
+    const onFormSubmit = (e) => {
+        e.preventDefault();
+
+        setEditing(prevState => !prevState);
+
+        dispatch(updateTodo(todo._id, text))
+    }
+
+    useEffect(() => {
+        // Your useEffect code here, or if you don't have any, you can leave it empty
+    }, [dispatch]); // Include 'dispatch' as a dependency 
+
+    return (
+        <li
+            className="task"
+            onClick={() => dispatch(toggleTodo(todo._id))}
+            style={{
+                textDecoration: todo?.done ? 'line-through' : '',
+                color: todo?.done ? '#bdc3c7' : '#34495e'
+            }}
+            data-testid="todo-test"
+        >
+            <span style={{ display: editing ? 'none' : '' }}>{todo?.data}</span>
+
+            <form
+                style={{ display: editing ? 'inline' : 'none' }}
+                onSubmit={onFormSubmit}
+            >
+                <input
+                    type="text"
+                    value={text}
+                    className="edit-todo"
+                    onChange={(e) => setText(e.target.value)}
+                />
+            </form>
+
+            <span className="icon" onClick={() => dispatch(deleteTodo(todo._id))}>
+                <i className="fas fa-trash" />
+            </span>
+            <span className="icon" onClick={() => setEditing(prevState => !prevState)}>
+                <i className="fas fa-pen" />
+            </span>
+        </li>
+    )
+}
+
+export default Todo;
